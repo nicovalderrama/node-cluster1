836b478f16fb489985c6fedd976a36aaf32cf377
nicolas <nicovalderrama2002@gmail.com>
2023-07-04 21:41:33 -0300

    creacion de admin

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -9,8 +9,18 @@ const sequelize = new Sequelize({
 const initializeDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("conexion exitosa");
     await sequelize.sync({ force: false });
+    const { User } = require("../models");
+    const admin = await User.findOne({ where: { name: "admin" } });
+    if (!admin) {
+      await User.create({
+        name: "admin",
+        password: "admin"
+      });
+      console.log("admin created");
+    } else {
+      console.log("admin already exists");
+    }
   } catch (error) {
     console.log("error at db inicialization", error);
   }
